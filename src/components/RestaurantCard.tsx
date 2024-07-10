064056d25e3e37fd8d787d2a2105e30811981c95
Mohd Fardeen <frad.fardeen@gmail.com>
2024-07-10 09:50:23 +0000

    add random image to restaurant card.

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { restaurant } from '@/lib/schema/restaurant';
 import RestaurantFormDialog from './RestaurantForm';
 import RestaurantDelistdialog from './RestaurantDelistdialog';
+import Image from 'next/image';
 
 export default function RestaurantCard({ rest, showOptions }: { rest: (typeof restaurant.$inferSelect), showOptions: boolean | undefined }) {
     return (
@@ -11,7 +12,10 @@ export default function RestaurantCard({ rest, showOptions }: { rest: (typeof re
                 <CardTitle className='text-2xl'>{rest.name}</CardTitle>
                 <CardDescription>Partner Since: {rest.dateAdded.toDateString()}</CardDescription>
             </CardHeader>
-            <CardContent className='line-clamp-4 h-24 mb-4'>{rest.description}</CardContent>
+            <CardContent className='flex flex-col items-center gap-5'>
+                <Image src={`/restaurant_${Math.floor(Math.random() * 10) + 1}.jpg`} width={640} height={640} alt='' className='rounded-lg' />
+                <span className='line-clamp-4 h-24'>{rest.description}</span>
+            </CardContent>
             <CardFooter className="flex justify-around">
                 {
                     showOptions && (
