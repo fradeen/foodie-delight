eb645d081b7681cc08c3a6bd26da31fbc08a2ef7
Mohd Fardeen <frad.fardeen@gmail.com>
2024-07-11 10:16:25 +0000

    fix searchbar scroll and links

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -2,24 +2,26 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { Popover, PopoverContent } from './ui/popover'
 import { Input } from './ui/input'
-import { PopoverAnchor } from '@radix-ui/react-popover'
+import { PopoverAnchor, PopoverClose } from '@radix-ui/react-popover'
 import { restaurant } from '@/lib/schema/restaurant'
 import { useDebounced } from '@/lib/hooks'
 import { searchRestarunt } from '@/lib/actions'
 import RestaurantSearchCard from './RestaurantSearchCard'
+import Link from 'next/link'
 
 export default function SearchBox() {
     const [open, setOpen] = useState(false)
     const [searchterm, setSerchTerm] = useState('')
     const [debouncedSearchterm, setDebouncedSearchTerm] = useState('')
     const [restaurants, setRestaurants] = useState<(typeof restaurant.$inferSelect)[]>([])
-    const debounceCallback = useCallback(() => { setDebouncedSearchTerm(searchterm) }, [searchterm])
+    const debounceCallback = useCallback(() => { setDebouncedSearchTerm(searchterm.trim()) }, [searchterm])
     useDebounced(debounceCallback, [searchterm], 1000)
     useEffect(() => {
-        searchRestarunt.bind(null, debouncedSearchterm)().then(list => {
-            if (list && list.length > 0)
-                setRestaurants(list)
-        })
+        if (debouncedSearchterm.length > 0)
+            searchRestarunt.bind(null, debouncedSearchterm)().then(list => {
+                if (list && list.length > 0)
+                    setRestaurants(list)
+            })
     }, [debouncedSearchterm])
     return (
         <Popover open={open && restaurants.length > 0} onOpenChange={setOpen}>
@@ -36,11 +38,18 @@ export default function SearchBox() {
             </PopoverAnchor>
             <PopoverContent
                 onOpenAutoFocus={(event) => event.preventDefault()}
-                className='w-[100svh] max-w-xs sm:max-w-sm md:max-w-screen-sm'
+                className='w-[100svh] max-h-screen max-w-xs sm:max-w-sm md:max-w-screen-sm overflow-y-auto'
             >
                 <div className='flex flex-col gap-2'>
                     {restaurants.map(restaurant => (
-                        <RestaurantSearchCard key={restaurant.id} rest={restaurant} />
+                        <PopoverClose key={restaurant.id} asChild onClick={() => {
+                            setSerchTerm('')
+                            setDebouncedSearchTerm('')
+                        }}>
+                            <Link href={`/restaurant/${restaurant.id}`}>
+                                <RestaurantSearchCard rest={restaurant} />
+                            </Link>
+                        </PopoverClose>
                     ))}
                 </div>
             </PopoverContent>
